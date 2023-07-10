1f9415ba2915390e19abc9c281596814a9e00980
irshad <irshadali0729@gmail.com>
2023-07-10 22:49:35 +0530

    feat(LeftNavbar) : Add left navbar and its functionality.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,12 @@
-const Feed=()=>{
-    return(
-        <div>
-            Feed
-        </div>
-    )
-}
+import React, { useContext, useState, useEffect } from "react";
+import LeftNav from "./LeftNav";
+import { Context } from "../context/contextApi";
+const Feed = () => {
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
